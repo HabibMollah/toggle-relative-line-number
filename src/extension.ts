f8a7775f1ae17b25cdef2729af3e64fcd2a8b940
HabibMollah <habib.prodev@gmail.com>
2024-09-11 21:33:43 +0600

    fix: fix grammars

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
-    "toggle-relative-line-number.toggle",
+    "toggle-relative-line-numbers.toggle",
     () => {
       const config = vscode.workspace.getConfiguration("editor");
       const currentLineNumbers = config.get("lineNumbers");
