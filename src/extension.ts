92ab358f552ca459592828b3f2918f9946b2723b
HabibMollah <habib.prodev@gmail.com>
2024-09-11 12:32:21 +0600

    feat: add toggle command to toggle relative numbers

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "toggle-relative-line-number.toggle",
+    () => {
+      const config = vscode.workspace.getConfiguration("editor");
+      const currentLineNumbers = config.get("lineNumbers");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "toggle-relative-line-number" is now active!');
+      config.update(
+        "lineNumbers",
+        currentLineNumbers === "on" ? "relative" : "on",
+        true
+      );
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('toggle-relative-line-number.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Toggle Relative Line Number!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
